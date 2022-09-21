2ecf6de056c278f5858c333cf634c28760a7d8ec
clg0803 <coder_guanchenlu@163.com>
2022-09-21 14:17:57 +0800

    add LET's ast

---
+++ ast/ast.go
@@ -0,0 +1,58 @@
+package ast
+
+import "github.com/clg0803/circus/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement 不产生值 Expression 产生值
+// let x = 5; 是 Statement ||  x 5 是 Expression
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// implememt Statement interface
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+// implement Expression interface
+// may not generate value
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
