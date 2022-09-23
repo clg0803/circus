89aaf87f1a7ff93c4607c15e08b38d30bdddd3fb
clg0803 <coder_guanchenlu@163.com>
2022-09-23 17:40:39 +0800

    parser's repl

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/clg0803/circus/lexer"
-	"github.com/clg0803/circus/token"
+	"github.com/clg0803/circus/parser"
 )
 
 const PROMPT = ">> "
@@ -23,9 +23,38 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
+}
+
+// repl/repl.go
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+    io.WriteString(out, MONKEY_FACE)
+    io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+    io.WriteString(out, " parser errors:\n")
+    for _, msg := range errors {
+        io.WriteString(out, "\t"+msg+"\n")
+    }
 }
